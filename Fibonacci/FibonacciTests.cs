ae89f31c6d0f66f1b3ea74f54d52480e2db5acc3
IIIByoIII <robertkiss26@gmail.com>
2017-10-14 11:34:56 +0300

    Added FastFibonacci7 test

--- Fibonacci/FibonacciTests.cs
+++ Fibonacci/FibonacciTests.cs
@@ -17,5 +17,25 @@ namespace Fibonacci
             if (n < 2) return n;
             return Fib(n - 1) + Fib(n - 2);
         }
+
+        [TestMethod]
+        public void FastFibonacci7()
+        {
+            Assert.AreEqual(13, FastFib(7));
+        }
+
+        int FastFib(int n)
+        {
+            int previous = 0;
+            return FastFib(n, ref previous);
+        }
+
+        int FastFib(int n, ref int previous)
+        {
+            if (n < 2) return n;
+            int beforePrevious = 0;
+            previous = FastFib(n - 1, ref beforePrevious);
+            return previous + beforePrevious;
+        }
     }
 }
