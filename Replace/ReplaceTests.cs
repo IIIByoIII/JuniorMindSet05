07e438d1bfec7de2df4f783226b5ab3b6c8eafb6
IIIByoIII <robertkiss26@gmail.com>
2017-10-14 13:13:05 +0300

    Added EasyReplaceX test

--- Replace/ReplaceTests.cs
+++ Replace/ReplaceTests.cs
@@ -7,8 +7,14 @@ namespace Replace
     public class ReplaceTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void EasyReplaceX()
         {
+            Assert.AreEqual("Hello world, beautifull world!", EasyReplaceChar("Hello X, beautifull X!", 'X', "world")); 
+        }
+
+        string EasyReplaceChar(string text, char c, string s)
+        {
+            return text.Replace(c.ToString(), s);
         }
     }
 }
