756b5d030a52cbf3ddca3433486e6fea8d70748b
IIIByoIII <robertkiss26@gmail.com>
2017-10-14 12:30:53 +0300

    Created new Replace project

---
+++ Replace/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Replace")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Replace")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2dcf3942-1e49-42d3-a77c-f7596cb2ff3f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Replace/ReplaceTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Replace
+{
+    [TestClass]
+    public class ReplaceTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
