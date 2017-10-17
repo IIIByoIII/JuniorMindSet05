79739cf1e134c6930a0583380d2d64688baf42f4
IIIByoIII <robertkiss26@gmail.com>
2017-10-17 21:33:19 +0300

    Added IndexOfLastNumber test

--- Calculator/CalculatorTests.cs
+++ Calculator/CalculatorTests.cs
@@ -7,8 +7,23 @@ namespace Calculator
     public class CalculatorTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void IndexOfLastNumber()
         {
+            object[] formula1 = new object[] {2, '+', 3.5, '*'};
+            object[] formula2 = new object[] {2, '+', 3, '*'};
+            Assert.AreEqual(2, LastIntIndex(formula1));
+            Assert.AreEqual(2, LastIntIndex(formula2));
+        }
+
+        int LastIntIndex(object[] f)
+        {
+            int intType = 1;
+            double doubleType = 1.0;
+            for (int i = f.Length - 1; i >= 0; i--) {
+                if ((f[i].GetType() == intType.GetType()) || (f[i].GetType() == doubleType.GetType()))
+                    return i;
+            }
+            return -1;
         }
     }
 }
