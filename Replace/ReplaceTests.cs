c3ed930ae4895b9983baeb5bdb4175a6bf2bd0ec
IIIByoIII <robertkiss26@gmail.com>
2017-10-17 21:00:19 +0300

    Modified ReplaceChar method and ReplaceO test

--- Replace/ReplaceTests.cs
+++ Replace/ReplaceTests.cs
@@ -26,21 +26,19 @@ namespace Replace
         [TestMethod]
         public void ReplaceO()
         {
-            Assert.AreEqual("Invalid replacement string and character combination", ReplaceChar("Hello X, beautifull X!", 'o', "world")); 
+            Assert.AreEqual("Hellworld Wworldrld!", ReplaceChar("Hello World!", 'o', "world")); 
         }
 
         string ReplaceChar(string text, char c, string s)
         {
-            string result1;
-            string result2;
-            if (s.IndexOf(c) >= 0)
-                return "Invalid replacement string and character combination";
-            int indexC = text.IndexOf(c);
-            if (indexC < 0)
-                return text;
-            result1 = String.Concat(text.Substring(0, indexC), s, text.Substring(indexC + 1));
-            result2 = ReplaceChar(result1, c, s);
-            return result2;
+            if (text.Length == 1)
+                if (text[0] == c)
+                    return s;
+                else
+                    return text;
+            if (text[0] == c)
+                return s + ReplaceChar(text.Substring(1), c, s);
+            return text[0].ToString() + ReplaceChar(text.Substring(1), c, s);
         }
     }
 }
