66e77741ccb443959111cd65839ef847b68d68c7
IIIByoIII <robertkiss26@gmail.com>
2017-10-18 20:16:49 +0300

    Added PrefixedCalculation1 test
    Also inserted spaces before/after curly brackets to correct folding in VIM

--- Calculator/CalculatorTests.cs
+++ Calculator/CalculatorTests.cs
@@ -9,8 +9,8 @@ namespace Calculator
         [TestMethod]
         public void IndexOfLastNumber()
         {
-            object[] formula1 = new object[] {2, '+', 3.5, '*'};
-            object[] formula2 = new object[] {2, '+', 3, '*'};
+            object[] formula1 = new object[] { 2, '+', 3.5, '*' };
+            object[] formula2 = new object[] { 2, '+', 3, '*' };
             Assert.AreEqual(2, LastIntIndex(formula1));
             Assert.AreEqual(2, LastIntIndex(formula2));
         }
@@ -29,7 +29,7 @@ namespace Calculator
         [TestMethod]
         public void IndexOfLastChar()
         {
-            object[] formula = new object[] {2, '+', '*', 3};
+            object[] formula = new object[] { 2, '+', '*', 3 };
             Assert.AreEqual(2, LastCharIndex(formula));
         }
 
@@ -41,6 +41,41 @@ namespace Calculator
                     return i;
             }
             return -1;
+        }
+
+        [TestMethod]
+        public void PrefixedCalculation1()
+        {
+            object[] formula = new object[] { '*', 3, 4 };
+            Assert.AreEqual(12.0, Calculate(formula));
+        }
+
+        double Calculate(object[] f)
+        {
+            var a = Convert.ToDouble(f[f.Length - 2]);
+            var b = Convert.ToDouble(f[f.Length - 1]);
+            int opIndex = LastCharIndex(f);
+            var operation = Convert.ToChar(f[opIndex]);
+            double result;
+            switch (operation) {
+                case '-':
+                    result = a - b;
+                    break;
+                case '*':
+                    result = a * b;
+                    break;
+                case '/':
+                    result = a / b;
+                    break;
+                default:
+                    result = a + b;
+                    break;
+            }
+            Array.Resize(ref f, opIndex + 1);
+            f[opIndex] = result;
+            if (f.Length == 1)
+                return Convert.ToDouble(f[0]);
+            return Calculate(f);
         }
     }
 }
