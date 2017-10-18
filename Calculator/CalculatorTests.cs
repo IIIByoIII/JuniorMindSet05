0a48cc8f8a87b21ae273aada31ac5f4de5c58c09
IIIByoIII <robertkiss26@gmail.com>
2017-10-18 20:18:49 +0300

    Removed IndexOfLastNumber test
    LastIntIndex method is not used after all.

--- Calculator/CalculatorTests.cs
+++ Calculator/CalculatorTests.cs
@@ -7,26 +7,6 @@ namespace Calculator
     public class CalculatorTests
     {
         [TestMethod]
-        public void IndexOfLastNumber()
-        {
-            object[] formula1 = new object[] { 2, '+', 3.5, '*' };
-            object[] formula2 = new object[] { 2, '+', 3, '*' };
-            Assert.AreEqual(2, LastIntIndex(formula1));
-            Assert.AreEqual(2, LastIntIndex(formula2));
-        }
-
-        int LastIntIndex(object[] f)
-        {
-            int intType = 1;
-            double doubleType = 1.0;
-            for (int i = f.Length - 1; i >= 0; i--) {
-                if ((f[i].GetType() == intType.GetType()) || (f[i].GetType() == doubleType.GetType()))
-                    return i;
-            }
-            return -1;
-        }
-
-        [TestMethod]
         public void IndexOfLastChar()
         {
             object[] formula = new object[] { 2, '+', '*', 3 };
