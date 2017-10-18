a6bf47a001ba56284c886af65278773186a7e19e
IIIByoIII <robertkiss26@gmail.com>
2017-10-18 20:37:23 +0300

    Added PrefixedCalculation2 test

--- Calculator/CalculatorTests.cs
+++ Calculator/CalculatorTests.cs
@@ -30,12 +30,19 @@ namespace Calculator
             Assert.AreEqual(12.0, Calculate(formula));
         }
 
+        [TestMethod]
+        public void PrefixedCalculation2()
+        {
+            object[] formula = new object[] { '*', '+', 1, 1, 2 };
+            Assert.AreEqual(4.0, Calculate(formula));
+        }
+
         double Calculate(object[] f)
         {
-            var a = Convert.ToDouble(f[f.Length - 2]);
-            var b = Convert.ToDouble(f[f.Length - 1]);
             int opIndex = LastCharIndex(f);
             var operation = Convert.ToChar(f[opIndex]);
+            var a = Convert.ToDouble(f[opIndex + 1]);
+            var b = Convert.ToDouble(f[opIndex + 2]);
             double result;
             switch (operation) {
                 case '-':
@@ -51,7 +58,9 @@ namespace Calculator
                     result = a + b;
                     break;
             }
-            Array.Resize(ref f, opIndex + 1);
+            for (int i = opIndex + 1; i < f.Length - 2; i++)
+                f[i] = f[i + 2];
+            Array.Resize(ref f, f.Length - 2);
             f[opIndex] = result;
             if (f.Length == 1)
                 return Convert.ToDouble(f[0]);
