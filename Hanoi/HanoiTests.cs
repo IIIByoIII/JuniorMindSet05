44d9753114cb6f106b6874ff8ca24c9362df4f84
IIIByoIII <robertkiss26@gmail.com>
2017-10-30 18:51:56 +0200

    Added new Hanoi1 and Hanoi2 tests

--- Hanoi/HanoiTests.cs
+++ Hanoi/HanoiTests.cs
@@ -7,8 +7,32 @@ namespace Hanoi
     public class HanoiTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Hanoi1()
         {
+            Assert.AreEqual("ac", HanoiMoves(1));
+        }
+
+        [TestMethod]
+        public void Hanoi2()
+        {
+            Assert.AreEqual("ab ac bc", HanoiMoves(2));
+        }
+
+        string HanoiMoves(int disks)
+        {
+            string result = "ac";
+            int totalMoves = (int)Math.Pow(2, disks) - 1;
+            for (int i = 0; i < totalMoves - 1; i += 2) {
+                string[] moves = result.Split(new char[] {' '});
+                int pos = (i * 6) + 1;
+                if (moves[i].IndexOf('a') == -1)
+                    result = result.Substring(0, pos) + "a " + moves[i] + " a" + result.Substring(pos);
+                else if (moves[i].IndexOf('b') == -1)
+                    result = result.Substring(0, pos) + "b " + moves[i] + " b" + result.Substring(pos);
+                else 
+                    result = result.Substring(0, pos) + "c " + moves[i] + " c" + result.Substring(pos);
+            }
+            return result;
         }
     }
 }
