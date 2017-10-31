3d4ea01fb5b7d39c1f929107e0cf4bf10bd7285d
IIIByoIII <robertkiss26@gmail.com>
2017-10-31 20:17:41 +0200

    Added PascalRow3Col2 test

--- PascalsTriangle/PascalsTriangleTests.cs
+++ PascalsTriangle/PascalsTriangleTests.cs
@@ -7,8 +7,16 @@ namespace PascalsTriangle
     public class PascalsTriangleTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void PascalRow3Col2()
         {
+            Assert.AreEqual(2, Pascal(3, 2));
+        }
+
+        int Pascal(int row, int col)
+        {
+            if ((col == 1) || (col == row))
+                return 1;
+            return Pascal(row - 1, col - 1) + Pascal(row - 1, col);
         }
     }
 }
