57cb82cf254e1ee766e26ca7046fb450d1901a69
IIIByoIII <robertkiss26@gmail.com>
2017-11-01 19:45:59 +0200

    Added Pascal3 test
    Also changed row and column numbering to use zero type index

--- PascalsTriangle/PascalsTriangleTests.cs
+++ PascalsTriangle/PascalsTriangleTests.cs
@@ -9,20 +9,37 @@ namespace PascalsTriangle
         [TestMethod]
         public void PascalRow3Col2()
         {
-            Assert.AreEqual(2, Pascal(3, 2));
+            Assert.AreEqual(2, Pascal(2, 1));
         }
 
         [TestMethod]
         public void PascalRow7Col4()
         {
-            Assert.AreEqual(20, Pascal(7, 4));
+            Assert.AreEqual(20, Pascal(6, 3));
         }
 
         int Pascal(int row, int col)
         {
-            if ((col == 1) || (col == row))
+            if ((col == 0) || (col == row))
                 return 1;
             return Pascal(row - 1, col - 1) + Pascal(row - 1, col);
+        }
+
+        [TestMethod]
+        public void Pascal3()
+        {
+            CollectionAssert.AreEqual(new string[] { "1", "11", "121" }, PascalTriangle(3));
+        }
+
+        string[] PascalTriangle(int level)
+        {
+            string[] result = new string[level];
+            for (int i = 0; i < level; i++) {
+                for (int j = 0; j <= i ; j++) {
+                    result[i] += Pascal(i, j).ToString();
+                }
+            }
+            return result;
         }
     }
 }
